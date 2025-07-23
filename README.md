c5ec1404741a17f99cd714c59baf4a8a089eeefe
JamezM546 <jmullins8452@gmail.com>
2025-07-23 20:18:57 +0100

    Initial Commit - keypad door lock project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rasberry Pi Keypad Door Lock System
+
+A door lock system using Rasberry Pi, a 4x4 keypad, a servo motor, and LED indicators. The lock is activated by entering a pin on the keypad.
+
+## Features
+
+- Keypad Input
+- Servo lock Mechnaism
+- Red/Green LED Indicators
+- Future: LCD + sound + emergecy failover
---
+++ keypd.py
@@ -0,0 +1,79 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# Keypad layout for 4x3
+KEYS = [
+    ['1','2','3'],
+    ['4','5','6'],
+    ['7','8','9'],
+    ['*','0','#']
+]
+
+ROW_PINS = [4, 17, 27, 22] # Yellow, Blue, Gray, White
+COL_PINS = [5, 6, 13] # Green, Purple, Black
+
+# Setup keypad pins
+for row in ROW_PINS:
+    GPIO.setup(row, GPIO.OUT)
+    GPIO.output(row, GPIO.LOW)
+
+for col in COL_PINS:
+    GPIO.setup(col, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Servo setup (update if your signal pin is different)
+SERVO_PIN = 18 # Default PWM pin
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+pwm = GPIO.PWM(SERVO_PIN, 50) # 50Hz
+pwm.start(0)
+
+def unlock_servo():
+    pwm.ChangeDutyCycle(7.5) # ~90°
+    time.sleep(1)
+    pwm.ChangeDutyCycle(2.5) # ~0°
+    time.sleep(0.5)
+
+def get_key():
+    for i, row in enumerate(ROW_PINS):
+        GPIO.output(row, GPIO.HIGH)
+        for j, col in enumerate(COL_PINS):
+            if GPIO.input(col) == GPIO.HIGH:
+                GPIO.output(row, GPIO.LOW)
+                return KEYS[i][j]
+        GPIO.output(row, GPIO.LOW)
+    return None
+
+CORRECT_CODE = "1234"
+code = ""
+EXIT_CODE = "0000"
+
+
+try:
+    print("Enter code (press '#' to submit):")
+    while True:
+        key = get_key()
+        if key:
+            print("Pressed:", key)
+            if key == '#':
+                if code == EXIT_CODE:
+                    print("Exit code entered. Stopping...")
+                    break
+                elif code == CORRECT_CODE:
+                    print("✅ Correct! Unlocking...")
+                    unlock_servo()
+                else:
+                    print("❌ Wrong code.")
+                code = ""
+            elif key == '*':
+                print("Code cleared")
+                code = ""
+            else:
+                code += key
+            time.sleep(0.3) # debounce delay
+except KeyboardInterrupt:
+    print("\nExiting...")
+finally:
+    pwm.stop()
+    GPIO.cleanup()
+    print("Servo and GPIO stopped cleangly.")
